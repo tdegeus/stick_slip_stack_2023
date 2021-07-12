97b242a7e576b77e009bdaaeebe7c13d5c77e0a4
Tom de Geus <tom@geus.me>
2021-07-12 18:06:57 +0200

    Making implementation viewParaview uniform

--- Run/viewParaview.py
+++ Run/viewParaview.py
@@ -1,20 +1,21 @@
 import argparse
-import os
-import tqdm
+import FrictionQPotFEM.UniformMultiLayerIndividualDrive2d as model
+import GMatElastoPlasticQPot.Cartesian2d as GMat
+import GooseFEM
 import h5py
 import numpy as np
+import os
 import prrng
-import GooseFEM as gf
-import GMatElastoPlasticQPot.Cartesian2d as gmat
-import FrictionQPotFEM.UniformMultiLayerIndividualDrive2d as model
+import tqdm
 import XDMFWrite_h5py as xh
 
 parser = argparse.ArgumentParser()
-parser.add_argument("files", type=str, nargs="*")
 parser.add_argument("-p", "--prefix", type=str, default="paraview")
+parser.add_argument("files", type=str, nargs="*")
 args = parser.parse_args()
-assert np.all([os.path.isfile(file) for file in args.files])
+assert np.all([os.path.isfile(os.path.realpath(file)) for file in args.files])
 assert len(args.files) > 0
+
 
 def read_epsy(data, N):
 
@@ -34,6 +35,29 @@ def read_epsy(data, N):
 
     return epsy
 
+
+def initsystem(data):
+
+    layers = data["/layers/stored"][...]
+
+    system = model.System(
+        data["/coor"][...],
+        data["/conn"][...],
+        data["/dofs"][...],
+        data["/iip"][...],
+        [data["/layers/{0:d}/elemmap".format(layer)][...] for layer in layers],
+        [data["/layers/{0:d}/nodemap".format(layer)][...] for layer in layers],
+        data["/layers/is_plastic"][...])
+
+    system.setDriveStiffness(data["/drive/k"][...], data["/drive/symmetric"][...])
+    system.setMassMatrix(data["/rho"][...])
+    system.setDampingMatrix(data["/damping/alpha"][...])
+    system.setElastic(data["/elastic/K"][...], data["/elastic/G"][...])
+    system.setPlastic(data["/cusp/K"][...], data["/cusp/G"][...], read_epsy(data, system.plastic().size))
+    system.setDt(data["/run/dt"][...])
+
+    return system
+
 for file in tqdm.tqdm(args.files):
 
     outbasename = "{0:s}_{1:s}".format(args.prefix, os.path.splitext(file)[0])
@@ -42,38 +66,7 @@ for file in tqdm.tqdm(args.files):
 
         with h5py.File(file, "r") as data:
 
-            layers = data["/layers/stored"][...]
-            elemmap = []
-            nodemap = []
-
-            for layer in layers:
-                elemmap += [data["/layers/{0:d}/elemmap".format(layer)][...]]
-                nodemap += [data["/layers/{0:d}/nodemap".format(layer)][...]]
-
-            system = model.System(
-                data["/coor"][...],
-                data["/conn"][...],
-                data["/dofs"][...],
-                data["/iip"][...],
-                elemmap,
-                nodemap,
-                data["/layers/is_plastic"][...])
-
-            system.setDriveStiffness(data["/drive/k"][...], data["/drive/symmetric"][...])
-            system.setMassMatrix(data["/rho"][...])
-            system.setDampingMatrix(data["/damping/alpha"][...])
-
-            system.setElastic(
-                data["/elastic/K"][...],
-                data["/elastic/G"][...])
-
-            system.setPlastic(
-                data["/cusp/K"][...],
-                data["/cusp/G"][...],
-                read_epsy(data, system.plastic().size))
-
-            system.setDt(data["/run/dt"][...])
-
+            system = initsystem(data)
             dV = system.quad().AsTensor(2, system.quad().dV())
             sig0 = data["/meta/normalisation/sig"][...]
 
@@ -84,14 +77,14 @@ for file in tqdm.tqdm(args.files):
 
             for inc in tqdm.tqdm(data["/stored"][...]):
 
-                system.layerSetUbar(
+                system.layerSetTargetUbar(
                     data["/drive/ubar/{0:d}".format(inc)][...],
                     data["/drive/drive"][...])
 
                 u = data["/disp/{0:d}".format(inc)][...]
                 system.setU(u)
 
-                Sig = gmat.Sigd(np.average(system.Sig() / sig0, weights=dV, axis=1))
+                Sig = GMat.Sigd(np.average(system.Sig() / sig0, weights=dV, axis=1))
 
                 output["/disp/{0:d}".format(inc)] = xh.as3d(u)
                 output["/sigd/{0:d}".format(inc)] = Sig
